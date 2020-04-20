9c3baf9ac47f7e4c3e55e45d17914c380e658e04
Oliver Degener <hg.otothel@gmail.com>
2020-04-21 01:05:21 +0200

    Make login request with axios

--- src/Login.js
+++ src/Login.js
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import axios from 'axios';
 
 class Login extends Component {
     constructor(props) {
@@ -16,7 +17,26 @@ class Login extends Component {
     }
 
     handleClick = () => {
-        console.log('Login clicked...');
+        console.log('Login clicked...', this.state);
+
+        const apiBaseUrl = "http://localhost:8080/api/";
+        const payload = {
+            "email": this.state.username,
+            "password": this.state.password
+        }
+
+        axios.post(apiBaseUrl + 'login', payload)
+            .then(function (response) {
+                console.log(response);
+                if (response.status === 200) {
+                    alert(`Login successful ${response.data.token}`)
+                } else {
+                    alert(`Error ${response.data.code}`);
+                }
+            })
+            .catch(function (error) {
+                alert(`${error}`);
+            });
     }
 
     render() {
@@ -33,16 +53,17 @@ class Login extends Component {
                     </AppBar>
 
                     <TextField
+                        type="email"
                         label="Email"
                         margin="normal"
-                        onChange={(event, newValue) => this.setState({ username: newValue })}
+                        onChange={(event) => { this.setState({ username: event.target.value }) }}
                     />
                     <br />
                     <TextField
                         type="password"
                         margin="normal"
                         label="Password"
-                        onChange={(event, newValue) => this.setState({ password: newValue })}
+                        onChange={(event) => this.setState({ password: event.target.value })}
                     />
                     <br />
                     <Button variant="contained" color="primary" style={style} onClick={(event) => this.handleClick(event)}>Login</Button>
