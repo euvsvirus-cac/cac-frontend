3ea23672de65be33fd5eb0acde2ba59742f152fd
Oliver Degener <hg.otothel@gmail.com>
2020-04-26 10:35:14 +0200

    Added Home route when logged in

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import LandingPage from "./containers/LandingPage";
 import Login from './containers/Login'
 import NotFound from "./containers/NotFound";
 import Register from './containers/Register'
 
+import { useAppContext } from './context/AppContext';
+
 export default function Routes() {
+    const { isAuthenticated } = useAppContext();
     return (
         <Switch>
             <Route exact path="/">
-                <Home />
+                {isAuthenticated ?
+                    <Home /> :
+                    <LandingPage />
+                }
             </Route>
+
             <Route exact path="/login">
                 <Login />
             </Route>
+
             <Route exact path="/register">
                 <Register />
             </Route>
+
             <Route>
                 <NotFound />
             </Route>
